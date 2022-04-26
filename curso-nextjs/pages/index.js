ded569c948fc8b81f700bdc44da587b33f3712bb
Maysavagee <mayte@graviti.mx>
2022-04-26 17:54:29 -0500

    Updated

---
+++ curso-nextjs/pages/index.js
@@ -0,0 +1,14 @@
+import Layout from '../components/layout';
+import Title from '../components/title';
+
+
+export default function Home(){
+    return(
+            <Layout>
+            <Title> Home Page </Title>
+            <p>
+                Aprendamos NextJS
+            </p>
+            </Layout>
+    )
+}
